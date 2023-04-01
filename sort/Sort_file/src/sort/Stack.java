7678fb7322e084340350a8582e9580cc1fee611d
hw130 <hewon130@gmail.com>
2023-04-02 02:03:53 +0900

    first commit

--- sort/Sort_file/src/sort/Stack.java
+++ sort/Sort_file/src/sort/Stack.java
@@ -1,9 +1,39 @@
 package sort;
 
+import java.util.Scanner;
+
 public class Stack {
 
 	public static void main(String[] args) {
-		int a, b;
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		int[] arr = new int[a];
+		int index = 0, temp;
+		
+		for(int i = 0; i < a; i++) {
+			arr[i] = scan.nextInt();
+		}
+		
+		for(int i = 0; i < a; i++) {
+			int min = 9999;
+			for(int j = 0; j < a; j++) {
+				if (min > arr[i]) {
+					index = j;
+					min = arr[i];
+					arr[i] = 
+				}
+			}
+			if (arr[i] < min) {
+				temp = arr[i];
+				arr[i] = min;
+				min = temp;
+			}
+			
+		}
+		
+		for(int i = 0; i < a; i++) {
+			System.out.println(arr[i]);
+		}
 
 	}
 
