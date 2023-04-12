02e9edf7e51b4b46f7e143bdd3ffc12384dd6a34
hw130 <hewon130@gmail.com>
2023-04-12 23:46:48 +0900

    default

--- sort/Sort_file/src/sort/Cutline.java
+++ sort/Sort_file/src/sort/Cutline.java
@@ -1,10 +1,31 @@
 package sort;
 
+import java.util.Scanner;
+
 public class Cutline {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int i, j, num,a, temp;
+		
+		Scanner scan = new Scanner(System.in);
+		num = scan.nextInt();
+		a = scan.nextInt();
+		
+		int[] arr = new int[num];
+		
+		for(i = 0; i < num; i++) {
+			arr[i] = scan.nextInt();
+		}
+		
+		for(i = 0; i < num; i++) {
+			for(j = 0; j = num - i; j++) {
+				if(arr[j] > arr[j + 1]) {
+					temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+				}
+			}
+		}
 	}
 
 }
