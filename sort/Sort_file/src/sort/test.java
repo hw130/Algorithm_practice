1ee50bccf9c13fce38734d3819e7605c0723e0db
hw130 <hewon130@gmail.com>
2023-05-25 23:42:53 +0900

    test

--- sort/Sort_file/src/sort/test.java
+++ sort/Sort_file/src/sort/test.java
@@ -1,6 +1,12 @@
 package sort;
 
 public class test {
+	class Solution {
+	    public int solution(int[] array) {
+	        int answer = array[array.length / 2];
+	        return answer;
+	    }
+	}
 
 	public static void main(String[] args) {
 		
