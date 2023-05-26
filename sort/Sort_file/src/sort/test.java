3288b4a8432271a59ae2cc09187379563b4e71a4
hw130 <hewon130@gmail.com>
2023-05-26 23:42:44 +0900

    배열 2배 만들기

--- sort/Sort_file/src/sort/test.java
+++ sort/Sort_file/src/sort/test.java
@@ -2,8 +2,19 @@ package sort;
 
 public class test {
 	class Solution {
-	    public int solution(int[] array) {
-	        int answer = array[array.length / 2];
+	    public int[] solution(int[] numbers) {
+	        int[] answer = new int[numbers.length];
+	        try{
+	            for(int i = 0; i < numbers.length; i++){
+	            answer[i] = numbers[i] * 2;
+	        }
+	        } catch(IndexOutOfBoundsException e) {
+
+				System.out.println(e);
+
+	        }
+	        
+	            
 	        return answer;
 	    }
 	}
