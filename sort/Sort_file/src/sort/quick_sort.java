579b77bdbc71b0e43736ff6e966df691dae518c5
hw130 <hewon130@gmail.com>
2023-05-27 14:13:06 +0900

    중앙값 구하기

--- sort/Sort_file/src/sort/quick_sort.java
+++ sort/Sort_file/src/sort/quick_sort.java
@@ -1,39 +1,32 @@
 package sort;
-/*
-int number = 10;
-int[] data = {1,10,5,8,7,6,4,3,2,9};
 
-public class quick_sort {
-	
-	public void quickSort(int data, int start, int end) {
-		if(start >= end) return;
-		
-		int key = start;
-		int i = start + 1;
-		int j = end;
-		int temp;
-		
-		while(i <= j) {
-			while(data[i] <= data[key]) {
-				i++;
-			}
-			while(data[j] >= data[key] && j > start) {
-				j--;
-			}
-			if(i > j) {
-				temp = data[j];
-				data[j] = data[key];
-				data[key] = temp;
-			}
-		}
-		
-	}
-
-	public static void main(String[] args) {
-		
-		
-		
-
-	}
-
-} */
+class Solution {
+    public static void quickSort(int[] arr, int start, int end){
+        if(start >= end) return;
+        int pivot = start;
+        int left = start + 1;
+        int right = end;
+        while(left <= right){
+            while(left <= right && arr[left] <= arr[pivot])left++;
+            while(right > start && arr[right] >= arr[pivot])right--;
+            
+            if(left >= right){
+                int temp = arr[pivot];
+                arr[pivot] = arr[right];
+                arr[right] = temp;
+            } else {
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+            }
+        }
+        quickSort(arr, start, right-1);
+        quickSort(arr, right+1, end);
+        
+    }
+    public int solution(int[] array) {
+        quickSort(array, 0, array.length - 1);
+        int answer = array[array.length / 2];
+        return answer;
+    }
+}
